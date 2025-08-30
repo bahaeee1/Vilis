dc0109ec2f5908a81e4bf0cd7d886be9d0e78491
bahaeee1 <bahaejifer2@gmail.com>
2025-08-30 23:41:49 +0100

    Create constants.js

---
+++ carsrent-client-upload-1756553160/src/constants.js
@@ -0,0 +1,10 @@
+// Catalog: well-known Moroccan cities
+export const MOROCCAN_CITIES = [
+  'Casablanca','Rabat','Salé','Fès','Marrakesh','Tangier','Agadir','Meknès','Oujda',
+  'Kenitra','Tétouan','Témara','Safi','Mohammedia','Khouribga','El Jadida','Béni Mellal',
+  'Nador','Taza','Settat','Larache','Ksar El Kebir','Khemisset','Guelmim','Ouarzazate',
+  'Tiznit','Errachidia','Berkane','Ouazzane','Taourirt','Sidi Slimane','Sidi Kacem',
+  'Taroudant','Laâyoune','Dakhla','Al Hoceïma','Chefchaouen','Ifrane','Midelt','Azrou'
+];
+
+export const FUEL_TYPES = ['diesel','petrol','hybrid','electric'];
